b6a24aa9e551eff240b8a81d1bcfa59060e3222e
niravlad26 <31075107+niravlad26@users.noreply.github.com>
2017-08-20 03:11:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text'
 
 element.innerHTML = "Ha Ha Ha";
 
-var elementimage = document.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.opacity = "40";
 };
