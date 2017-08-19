9b3dbb9d1270c6ba89916dbf1d37d9c6856e799b
niravlad26 <31075107+niravlad26@users.noreply.github.com>
2017-08-20 03:25:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ var element = document.getElementById('main-text'
 element.innerHTML = "Ha Ha Ha";
 
 var img = document.getElementById('madi');
-img.hover = function() { 
+img.hoverover = function() { 
     img.style.zoom = 1.5;
 };
